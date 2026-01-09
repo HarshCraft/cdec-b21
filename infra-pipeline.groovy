83e476fd239a36fef490182ae111bcde7a763a90
HarshCraft <harshindia8637@gmail.com>
2026-01-09 22:18:12 +0530

    deploy commit 6

--- infra-pipeline.groovy
+++ infra-pipeline.groovy
@@ -1,35 +1,23 @@
-pipeline {
-  agent any
-
-  stages {
-    stage('PULL') {
-      steps {
-        git 'https://github.com/HarshCraft/cdec-b21.git'
-      }
-    }
+def runInfra() {
 
     stage('TF INIT') {
-      steps {
         dir('terraform') {
-          sh 'terraform init'
+            sh 'terraform init'
         }
-      }
     }
 
     stage('TF PLAN') {
-      steps {
         dir('terraform') {
-          sh 'terraform plan'
+            sh 'terraform plan'
         }
-      }
     }
 
     stage('TF APPLY') {
-      steps {
         dir('terraform') {
-          sh 'terraform apply -auto-approve'
+            sh 'terraform apply -auto-approve'
         }
-      }
     }
-  }
+
 }
+
+return this
